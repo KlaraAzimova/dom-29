b898fb8050a5ca1f8dad4a854a39acb5e393e93f
Klara <azimovaklara18@gmail.com>
2022-03-19 16:45:24 +0300

    changs

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,12 +1,15 @@
 package com.company;
 
+import java.io.FilterOutputStream;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
-        ArrayList<Integer> arrayList1 = new ArrayList<Integer>(List.of(98, 74, 56, 32, 14, 52, 36, 78, 96,9632,8521,7654));
+        List<Integer> arrayList1 = new ArrayList<Integer>(List.of(98, 74, 56, 32, 14, 52, 36, 78, 96));
+        arrayList1.stream().filter(x -> x % 2 == 0).forEach(System.out::println);
         arrayList1.stream().filter(x -> x % 2 == 0).map(x -> x * x).max(Integer::compare).ifPresent(System.out::println);
+
     }
 }
